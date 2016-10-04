2d9ecf7e3e5155fa19fe8d826f5385fcbbaca710
zhuang donglin <zhuang donglin>
2016-10-04 23:45:20 +0800

    change log

--- tickets.py
+++ tickets.py
@@ -17,10 +17,27 @@ Opthons:
 Example tickets beijing shanghai 2016-10-1
 """
 from docopt import docopt
-
+from pprint import pprint
+from station_map import stations
+from prettytable import PrettyTable
 import requests
 
-from station_map import stations
+class TrainCollection(object):
+    #显示车次、出发/到达站、出发/到达时间、历时、一等座、二等座、软卧、硬卧、硬座
+    header = 'train station time duration first second softsleep headsleep hardsit'.split()
+    def __init__(self, rows):
+        self.rows = rows
+    def __get_duration(self, row):
+        #获取车次运行时间
+        duration = row['lishi'].replace(':', 'h') + 'm'
+        if duration.startswith('00'):
+            return duration[4:]
+        if duration.startswith('0'):
+            return duration[1:]
+        return duration
+    
+    
+
 def cli():
     #cmd interface
     arguments = docopt(__doc__)
@@ -31,6 +48,6 @@ def cli():
         date, from_station, to_station
     )
     r = requests.get(url,verify=False)
-    print(r.json()['start_train_date'])
+    pprint(r.json(), indent=2)
 if __name__ == '__main__' :
     cli()
